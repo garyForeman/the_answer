0a1c325ad94d4d8b521e13d042fd62a5d5ec8c8e
Gary Foreman <gjman@MacBook-Pro-3.local>
2018-07-15 14:35:08 -0700

    Add test for positional arguments

--- test_the_answer.py
+++ test_the_answer.py
@@ -3,3 +3,7 @@ from the_answer import the_answer
 def test_the_answer_no_args():
     answer = the_answer()
     assert answer == 42
+
+def test_the_answer_args():
+    answer = the_answer('life', 'universe', 'everything')
+    assert answer == 42
