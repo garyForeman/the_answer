6cabe963b8f1e99cc4367f5e455a1d598be1e39f
Gary Foreman <gjman@MacBook-Pro-3.local>
2018-07-24 19:33:54 -0700

    Add kwargs tests

--- test_the_answer.py
+++ test_the_answer.py
@@ -7,3 +7,11 @@ def test_the_answer_no_args():
 def test_the_answer_args():
     answer = the_answer('life', 'universe', 'everything')
     assert answer == 42
+
+def test_the_answer_kwargs():
+    answer = the_answer(rush=2112, yes='roundabout')
+    assert answer == 42
+
+def test_the_answer_both():
+    answer = the_answer('foo', 'bar', bat='baz')
+    assert answer == 42
